9354fc1c7ec19f5e9d81c01acf2efd98017722bd
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-05-01 01:29:37 -0300

    add DemoBean

--- src/main/java/com/tutorial/npspring/app/mail/MailConfig.java
+++ src/main/java/com/tutorial/npspring/app/mail/MailConfig.java
@@ -1,6 +1,5 @@
 package com.tutorial.npspring.app.mail;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +7,11 @@ import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 public class MailConfig {
+	
+	@Bean
+	public DemoBean demoBean() {
+		return new DemoBean();
+	}
 	
 	@Bean
 	@ConditionalOnProperty(
@@ -22,6 +26,7 @@ public class MailConfig {
 	@Bean
 	@ConditionalOnProperty(name="spring.mail.host")
 	public MailSender smtpMailSender(JavaMailSender javaMailSender) {
+		demoBean().foo();
 		SmtpMailSender mailSender = new SmtpMailSender();
 		mailSender.setJavaMailSender(javaMailSender);
 		return mailSender;
